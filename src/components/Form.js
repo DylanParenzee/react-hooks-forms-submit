9aa81dbef077ef14205e05a2cca1cca2bc3f3bdb
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-14 20:57:11 +1000

    added submit handliing on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [lastName, setLastName] = useState("Smith");
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,8 +12,15 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    const formData = { firstName: firstName, lastName: lastName };
+    setFirstName("");
+    setLastName("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
